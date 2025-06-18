96464129a8e4a3fa50e04125935266e9f5b49c6c
Naim Rahmouni <naimrahmouni1998@gmail.com>
2025-06-18 11:39:05 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -13,43 +13,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer, webpack }) => {
-    // Suppress warnings for optional modules that may not be installed
-    // or for modules that generate noise during the build.
-
-    // For "@opentelemetry/exporter-jaeger":
-    // This is an optional OpenTelemetry exporter. Genkit/OpenTelemetry might try to
-    // dynamically require it based on environment variables. If it's not present
-    // and not configured for use, the application should still run.
-    // We use IgnorePlugin to prevent Webpack from erroring or warning on its absence.
-    if (!config.plugins) {
-      config.plugins = [];
-    }
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^@opentelemetry\/exporter-jaeger$/,
-      })
-    );
-
-    // For "require.extensions is not supported by webpack" from Handlebars:
-    // This warning is common when Handlebars (a CJS module) is bundled by Webpack.
-    // It's generally safe to ignore if Handlebars itself works in your Genkit flows.
-    if (!config.ignoreWarnings) {
-      config.ignoreWarnings = [];
-    }
-    config.ignoreWarnings.push(
-      {
-        module: /node_modules\/handlebars\//,
-        message: /require\.extensions/,
-      },
-      {
-        file: /node_modules\/handlebars\//,
-        message: /require\.extensions/,
-      }
-    );
-
-    return config;
-  },
+  // The webpack function is removed as it's not used by Turbopack
+  // and was causing the warning: "Webpack is configured while Turbopack is not".
+  // If warnings previously suppressed by this block (e.g., for OpenTelemetry or Handlebars)
+  // reappear and are problematic, they may need Turbopack-specific solutions or to be accepted.
 };
 
 export default nextConfig;
