08a986ca46af0f59833f1205e7be06850e1c7f63
Naim Rahmouni <naimrahmouni1998@gmail.com>
2025-07-18 21:31:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- Obralink/src/app/page.tsx
+++ Obralink/src/app/page.tsx
@@ -1,3 +1,4 @@
+
 // This file is no longer needed as the root page is now handled by /app/[locale]/page.tsx
 // It can be deleted, but keeping it empty avoids breaking any potential (though unlikely) imports.
 export default function ObsoleteRootPage() {
